d3a619daa360bcb5b3cc968fbe57f99687af1d68
MYuPopov <beemax@bk.ru>
2021-11-25 22:32:45 +0300

    Первый второй коммит задания 1

--- script.js
+++ script.js
@@ -1,2 +1,9 @@
-const a = 1;
-let b = "вторая переменная";
+const title = "gitHub";
+let screens;
+const screenPrice = 80;
+let rollback;
+const fullPrice = 100;
+let adaptive;
+
+alert(title);
+console.log("fullPrice: ", fullPrice);
