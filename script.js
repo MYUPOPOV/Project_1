e8eaf6b051ac360ba0da05ea34dea7a6f8ef583f
MYuPopov <beemax@bk.ru>
2021-11-26 10:11:28 +0300

    Урок 2 Простое задание выведение в консоль переменных, работа со строками

--- script.js
+++ script.js
@@ -1,4 +1,17 @@
-let title, screens, screenPrice, rollback, fullPrice, adaptive;
+let title = "Project_1";
+let screen = "Простые, Сложные, Интерактивные";
+let screenPrice = 1500;
+let rollback = 35;
+let fullPrice = 50500;
+let adaptive = true;
 
-alert("Модальное окно alert");
-console.log("console.log");
+console.log(title, fullPrice, adaptive);
+console.log(screen.length);
+console.log("Стоимость верстки экранов: " + screenPrice + " рублей");
+console.log("Стоимость разработки сайта: " + fullPrice + " рублей");
+console.log(screen.toLowerCase().split(", "));
+console.log(
+	"Процент отката посреднику за работу: " +
+		(fullPrice * rollback) / 100 +
+		" рублей"
+);
