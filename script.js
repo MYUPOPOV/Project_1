1890d53ac6f2f57fd669c27dc4d2edf4fe6034a7
MYuPopov <beemax@bk.ru>
2021-11-26 17:08:01 +0300

    Исправления после проверки куратором

--- script.js
+++ script.js
@@ -1,15 +1,17 @@
 let title = "Project_1";
-let screen = "Простые, Сложные, Интерактивные";
+let screens = "Простые, Сложные, Интерактивные";
 let screenPrice = 1500;
 let rollback = 35;
 let fullPrice = 50500;
 let adaptive = true;
 
-console.log(title, fullPrice, adaptive);
-console.log(screen.length);
+console.log(typeof title);
+console.log(typeof fullPrice);
+console.log(typeof adaptive);
+console.log(screens.length);
 console.log("Стоимость верстки экранов: " + screenPrice + " рублей");
 console.log("Стоимость разработки сайта: " + fullPrice + " рублей");
-console.log(screen.toLowerCase().split(", "));
+console.log(screens.toLowerCase().split(", "));
 
 console.log(
 	"Процент отката посреднику за работу: " +
