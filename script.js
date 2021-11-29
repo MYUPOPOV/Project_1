2486279c9b73bd793de89bda5e2a90d433219cbc
MYuPopov <beemax@bk.ru>
2021-11-29 12:07:49 +0300

    Добавлены: опрос заказчика, расчет скидки

--- script.js
+++ script.js
@@ -1,9 +1,33 @@
-let title = "Project_1";
-let screens = "Простые, Сложные, Интерактивные";
-let screenPrice = 1500;
-let rollback = 35;
-let fullPrice = 50500;
-let adaptive = true;
+let title = prompt("Как называется ваш проект?");
+let screens = prompt(
+	"Какие типы экранов нужно разработать?",
+	"Простые, Сложные, Интерактивные"
+);
+let screenPrice = prompt("Сколько будет стоить данная работа?", "12000");
+let rollback = 20;
+
+let adaptive = confirm("Нужен ли адаптив на сайте?");
+
+let service1 = prompt("Какой дополнительный тип услуг нужен?");
+let servicePrice1 = prompt("Сколько это будет стоить?", "5000");
+
+let service2 = prompt("Какой ещё дополнительный тип услуг нужен?");
+let servicePrice2 = prompt("Сколько это будет стоить?", "7000");
+
+let fullPrice = +screenPrice + +servicePrice1 + +servicePrice2;
+
+let servicePercentPrice = Math.ceil(fullPrice - (fullPrice * +rollback) / 100);
+console.log("servicePercentPrice: ", servicePercentPrice);
+
+if (fullPrice > 30000) {
+	console.log("Даем скидку в 10%");
+} else if (fullPrice > 15000 && fullPrice <= 30000) {
+	console.log("Даем скидку в 5%");
+} else if (fullPrice > 0 && fullPrice <= 15000) {
+	console.log("Скидка не предусмотрена");
+} else {
+	console.log("Что-то пошло не так");
+}
 
 console.log(typeof title);
 console.log(typeof fullPrice);
