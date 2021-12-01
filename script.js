005ef527c24558b7752aa09cf483f872035485e2
MYuPopov <beemax@bk.ru>
2021-12-01 13:51:19 +0300

    screenPrice через цикл; валидация числа getAllServicePrice

--- script.js
+++ script.js
@@ -1,50 +1,79 @@
-/* Урок 4 */
+/* Урок 5 */
 /* Блок объявления переменных */
-let title = prompt("Как называется ваш проект?", "Проект1");
-let screens = prompt(
-	"Какие типы экранов нужно разработать?",
-	"Простые, Сложные, Интерактивные"
-);
-let screenPrice = prompt("Сколько будет стоить данная работа?", "12000");
-let rollback = 20;
-let adaptive = confirm("Нужен ли адаптив на сайте?");
-let service1 = prompt(
-	"Какой дополнительный тип услуг нужен?",
-	"Разработка модального окна"
-);
-let servicePrice1 = prompt("Сколько это будет стоить?", "5000");
-let service2 = prompt(
-	"Какой ещё дополнительный тип услуг нужен?",
-	"Разработка корзины"
-);
-let servicePrice2 = prompt("Сколько это будет стоить?", "7000");
+// Переменные asking()
+let title;
+let screens;
+let screenPrice;
+let adaptive;
+// getAllServicePrice()
+let service1;
+let service2;
 let allServicePrices;
+// getFullPrice()
 let fullPrice;
+// getRollbackMessage())
+let rollback = 20;
 let servicePercentPrice;
 
 /* Блок описания функций */
 
-const getAllServicePrice = function (price1, price2) {
-	return +price1 + +price2;
-}; // Тип - function expression (1)
+// Универсальная функция для проверки на число (Урок 5. Практика)
+const isNumber = function (num) {
+	return !isNaN(parseFloat(num)) && isFinite(num);
+};
 
+// Запрос значений переменных с валидацией
+const asking = () => {
+	do {
+		screenPrice = +prompt("Сколько будет стоить данная работа?", "12000").trim();
+	} while (!isNumber(screenPrice));
+	const fastAsking = confirm("Вы хотите быстро заполнить переменные title, screens, adaptive ?"); // Для ускорения тестирования
+	title = fastAsking ? "КалькуляторВёрстки" : prompt("Как называется ваш проект?", "КалькуляторВёрстки");
+	screens = fastAsking ? "Простые, Сложные" : prompt("Какие типы экранов нужно разработать?", "Простые, Сложные, Интерактивные");
+	adaptive = fastAsking ? true : confirm("Нужен ли адаптив на сайте?");
+};
+
+// Расчет стоимости дополнительный услуг
+const getAllServicePrice = () => {
+	let sum = 0;
+	let increment;
+	const fastAsking = confirm("Вы хотите быстро заполнить переменные service1, service2 ?"); // Для ускорения тестирования
+	for (let i = 0; i < 2; i++) {
+		if (i === 0) {
+			service1 = fastAsking ? "Разработка модального окна" : prompt("Какой дополнительный тип услуг нужен?", "Разработка модального окна");
+		} else {
+			service2 = fastAsking ? "Разработка корзины" : prompt("Какой ещё дополнительный тип услуг нужен?", "Разработка корзины");
+		}
+		do {
+			increment = +prompt("Сколько это будет стоить?", "5000").trim();
+		} while (!isNumber(increment));
+		sum += increment;
+	}
+	return sum;
+};
+
+// Расчет полной цены
 function getFullPrice(screenPrice, allServicePrices) {
 	return +screenPrice + +allServicePrices;
-} // Тип - function declaration (2)
+}
 
+// Валидация названия проекта
 const getTitle = (title) => {
 	let str = title.trim();
-	return str[0].toUpperCase() + str.toLowerCase().slice(1); // Еще можно регулярками это сделать)
-}; // Так как в (3) не указан требуемый тип, решил объявить ее через стрелочный тип
+	return str[0].toUpperCase() + str.toLowerCase().slice(1); // ещё можно через regexp
+};
 
+// Расчитываем цену за вычетом отката посреднику
 const getServicePercentPrices = (fullPrice, rollback) => {
 	return Math.ceil(fullPrice - (fullPrice * +rollback) / 100);
 };
 
+// Показываем тип переменных
 const showTypeOf = (variable) => {
 	console.log(variable + ", тип данных: " + typeof variable);
-}; // По видео "Практика", но решил использовать стрелочную функцию
+};
 
+// Показываем скидку
 const getRollbackMessage = (price) => {
 	if (price > 30000) {
 		return "Даем скидку в 10%";
@@ -55,10 +84,15 @@ const getRollbackMessage = (price) => {
 	} else {
 		return "Что-то пошло не так";
 	}
-}; // По видео "Практика", но решил использовать стрелочную функцию
+};
 
 /* Блок выполнения команд */
-allServicePrices = getAllServicePrice(servicePrice1, servicePrice2);
+asking();
+allServicePrices = getAllServicePrice();
+
+console.log("~ screenPrice", screenPrice); // В этом коммите оставлю для упрощения тестирования
+console.log("~ allServicePrices", allServicePrices);
+
 fullPrice = getFullPrice(screenPrice, allServicePrices);
 servicePercentPrice = getServicePercentPrices(fullPrice, rollback);
 title = getTitle(title);
@@ -67,12 +101,15 @@ showTypeOf(fullPrice);
 showTypeOf(adaptive);
 console.log(screens.toLowerCase().split(", "));
 console.log(getRollbackMessage(fullPrice));
-console.log(
-	"Стоимость за вычетом процента отката посреднику: ",
-	servicePercentPrice
-);
+console.log("Стоимость за вычетом процента отката посреднику: ", servicePercentPrice);
 
-/* console.log(screens.length);
+/* Можно удалить:
+
+const getAllServicePrice = function (price1, price2) {
+return +price1 + +price2;
+function expression
+
+console.log(screens.length);
 console.log("Стоимость верстки экранов: " + screenPrice + " рублей");
 console.log("Стоимость разработки сайта: " + fullPrice + " рублей");
 console.log(
