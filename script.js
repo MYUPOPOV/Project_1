bc5784300b96bbe30bd33b4d432a24f0819fca41
MYuPopov <beemax@bk.ru>
2021-12-03 14:09:52 +0300

    Создан объект. Все функции перемещены в него как методы

--- script.js
+++ script.js
@@ -1,120 +1,117 @@
-/* Урок 5 */
-/* Блок объявления переменных */
-// Переменные asking()
-let title;
-let screens;
-let screenPrice;
-let adaptive;
-// getAllServicePrice()
-let service1;
-let service2;
-let allServicePrices;
-// getFullPrice()
-let fullPrice;
-// getRollbackMessage())
-let rollback = 20;
-let servicePercentPrice;
+/* Урок 7 */
+/* Блок объявления переменных  */
 
-/* Блок описания функций */
+const appData = {
+	title: "", // Переменные asking()
+	screens: "",
+	screenPrice: 0,
+	adaptive: true,
+	service1: 0, // getAllServicePrice()
+	service2: 0,
+	allServicePrices: 0,
+	fullPrice: 0, // getFullPrice()
+	servicePercentPrice: 0, // getRollbackMessage())
+	rollback: 20,
 
-// Универсальная функция для проверки на число (Урок 5. Практика)
-const isNumber = function (num) {
-	return !isNaN(parseFloat(num)) && isFinite(num);
-};
-
-// Запрос значений переменных с валидацией
-const asking = () => {
-	do {
-		screenPrice = +prompt("Сколько будет стоить данная работа?", "12000");
-	} while (!isNumber(screenPrice));
-
-	const fastAsking = confirm("Вы хотите быстро заполнить переменные title, screens, adaptive ?"); // Для ускорения тестирования
-	title = fastAsking ? "КалькуляторВёрстки" : prompt("Как называется ваш проект?", "КалькуляторВёрстки");
-	screens = fastAsking ? "Простые, Сложные" : prompt("Какие типы экранов нужно разработать?", "Простые, Сложные, Интерактивные");
-	adaptive = fastAsking ? true : confirm("Нужен ли адаптив на сайте?");
-};
-
-// Расчет стоимости дополнительный услуг
-const getAllServicePrice = () => {
-	let sum = 0;
-	let increment;
-	const fastAsking = confirm("Вы хотите быстро заполнить переменные service1, service2 ?"); // Для ускорения тестирования
-	for (let i = 0; i < 2; i++) {
-		if (i === 0) {
-			service1 = fastAsking ? "Разработка модального окна" : prompt("Какой дополнительный тип услуг нужен?", "Разработка модального окна");
-		} else {
-			service2 = fastAsking ? "Разработка корзины" : prompt("Какой ещё дополнительный тип услуг нужен?", "Разработка корзины");
-		}
+	// Запрос значений переменных с валидацией
+	asking: function () {
 		do {
-			increment = +prompt("Сколько это будет стоить?", "5000");
-		} while (!isNumber(increment));
-		sum += increment;
-	}
-	return sum;
+			appData.screenPrice = +prompt("Сколько будет стоить данная работа?", "12000");
+		} while (!appData.isNumber(appData.screenPrice));
+		// Для ускорения тестирования:
+		const fastAsking = confirm("Вы хотите быстро заполнить переменные title, screens, adaptive ?");
+		appData.title = fastAsking ? "КалькуляторВёрстки" : prompt("Как называется ваш проект?", "КалькуляторВёрстки");
+		appData.screens = fastAsking ? "Простые, Сложные" : prompt("Какие типы экранов нужно разработать?", "Простые,      Сложные,    Интерактивные");
+		appData.adaptive = fastAsking ? true : confirm("Нужен ли адаптив на сайте?");
+	},
+
+	// Универсальный метод для проверки на число (Урок 5. Практика)
+	isNumber: function (num) {
+		return !isNaN(parseFloat(num)) && isFinite(num);
+	},
+
+	// Расчет стоимости дополнительный услуг
+	getAllServicePrice: () => {
+		let sum = 0;
+		let increment;
+		const fastAsking = confirm("Вы хотите быстро заполнить переменные service1, service2 ?"); // Для ускорения тестирования
+		for (let i = 0; i < 2; i++) {
+			if (i === 0) {
+				appData.service1 = fastAsking ? "Разработка модального окна" : prompt("Какой дополнительный тип услуг нужен?", "Разработка модального окна");
+			} else {
+				appData.service2 = fastAsking ? "Разработка корзины" : prompt("Какой ещё дополнительный тип услуг нужен?", "Разработка корзины");
+			}
+			do {
+				increment = +prompt("Сколько это будет стоить?", "5000");
+			} while (!appData.isNumber(increment));
+			sum += increment;
+		}
+		return sum;
+	},
+
+	// Расчет полной цены
+	getFullPrice: function (screenPrice, allServicePrices) {
+		return +screenPrice + +allServicePrices;
+	},
+
+	// Валидация названия проекта
+	getTitle: (title) => {
+		let str = title.trim();
+		return str[0].toUpperCase() + str.toLowerCase().slice(1); // ещё можно через regexp
+	},
+
+	// Расчитываем цену за вычетом отката посреднику
+	getServicePercentPrices: (fullPrice, rollback) => {
+		return Math.ceil(fullPrice - (fullPrice * +rollback) / 100);
+	},
+
+	// Показываем тип переменных
+	showTypeOf: (variable) => {
+		console.log(variable + ", тип данных: " + typeof variable);
+	},
+
+	// Показываем скидку
+	getRollbackMessage: (price) => {
+		if (price > 30000) {
+			return "Даем скидку в 10%";
+		} else if (price > 15000 && price <= 30000) {
+			return "Даем скидку в 5%";
+		} else if (price > 0 && price <= 15000) {
+			return "Скидка не предусмотрена";
+		} else {
+			return "Что-то пошло не так";
+		}
+	},
+
+	/* Блок отображения логов */
+	logger: () => {
+		console.log(appData.screens.toLowerCase().split(", "));
+		console.log(appData.getRollbackMessage(appData.fullPrice));
+		console.log("Стоимость за вычетом процента отката посреднику: ", appData.servicePercentPrice);
+		console.log("");
+
+		console.log("Типы данных:");
+		appData.showTypeOf(appData.title);
+		appData.showTypeOf(appData.fullPrice);
+		appData.showTypeOf(appData.adaptive);
+		console.log("");
+
+		console.log("Дополнительные логи:");
+		console.log("~ screenPrice", appData.screenPrice);
+		console.log("~ allServicePrices", appData.allServicePrices);
+		console.log("~ appData.fullPrice", appData.fullPrice);
+		console.log("~ appData.servicePercentPrice", appData.servicePercentPrice);
+	},
+
+	/* Блок выполнения команд */
+	start: () => {
+		appData.asking();
+		appData.allServicePrices = appData.getAllServicePrice();
+		appData.fullPrice = appData.getFullPrice(appData.screenPrice, appData.allServicePrices);
+		appData.servicePercentPrice = appData.getServicePercentPrices(appData.fullPrice, appData.rollback);
+		appData.title = appData.getTitle(appData.title);
+		appData.logger();
+	},
 };
 
-// Расчет полной цены
-function getFullPrice(screenPrice, allServicePrices) {
-	return +screenPrice + +allServicePrices;
-}
-
-// Валидация названия проекта
-const getTitle = (title) => {
-	let str = title.trim();
-	return str[0].toUpperCase() + str.toLowerCase().slice(1); // ещё можно через regexp
-};
-
-// Расчитываем цену за вычетом отката посреднику
-const getServicePercentPrices = (fullPrice, rollback) => {
-	return Math.ceil(fullPrice - (fullPrice * +rollback) / 100);
-};
-
-// Показываем тип переменных
-const showTypeOf = (variable) => {
-	console.log(variable + ", тип данных: " + typeof variable);
-};
-
-// Показываем скидку
-const getRollbackMessage = (price) => {
-	if (price > 30000) {
-		return "Даем скидку в 10%";
-	} else if (price > 15000 && price <= 30000) {
-		return "Даем скидку в 5%";
-	} else if (price > 0 && price <= 15000) {
-		return "Скидка не предусмотрена";
-	} else {
-		return "Что-то пошло не так";
-	}
-};
-
-/* Блок выполнения команд */
-asking();
-allServicePrices = getAllServicePrice();
-
-console.log("~ screenPrice", screenPrice); // В этом коммите оставлю для упрощения тестирования
-console.log("~ allServicePrices", allServicePrices);
-
-fullPrice = getFullPrice(screenPrice, allServicePrices);
-servicePercentPrice = getServicePercentPrices(fullPrice, rollback);
-title = getTitle(title);
-showTypeOf(title);
-showTypeOf(fullPrice);
-showTypeOf(adaptive);
-console.log(screens.toLowerCase().split(", "));
-console.log(getRollbackMessage(fullPrice));
-console.log("Стоимость за вычетом процента отката посреднику: ", servicePercentPrice);
-
-/* Можно удалить:
-
-const getAllServicePrice = function (price1, price2) {
-return +price1 + +price2;
-function expression
-
-console.log(screens.length);
-console.log("Стоимость верстки экранов: " + screenPrice + " рублей");
-console.log("Стоимость разработки сайта: " + fullPrice + " рублей");
-console.log(
-	"Процент отката посреднику за работу: " +
-		(fullPrice * rollback) / 100 +
-		" рублей"
-); */
+appData.start();
